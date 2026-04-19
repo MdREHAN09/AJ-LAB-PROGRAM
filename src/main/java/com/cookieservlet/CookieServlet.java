2d9a8bee2d24a9e0e456f05973940e5ed45c6e53
MdREHAN09 <mohammedrehan0924@gmail.com>
2026-04-19 16:15:23 +0530

    cookie

--- src/main/java/com/cookieservlet/CookieServlet.java
+++ src/main/java/com/cookieservlet/CookieServlet.java
@@ -10,71 +10,71 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/CookieServlet")
-
 public class CookieServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        // 🔹 Get username from request (after submit)
         String userName = request.getParameter("userName");
 
-        if (userName != null && !userName.isEmpty()) {
-            Cookie userCookie = new Cookie("user", userName);
-            userCookie.setMaxAge(60); // ✅ 60 sec = 1 minute
-            response.addCookie(userCookie);
-
-            // create count cookie first time
-            Cookie countCookie = new Cookie("count", "1");
-            countCookie.setMaxAge(60);
-            response.addCookie(countCookie);
-        }
-
-        // 🔹 Read cookies
+        // Read cookies
         Cookie[] cookies = request.getCookies();
         String existingUser = null;
-        int count = 0;
+        int visitCount = 0;
 
         if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("user")) {
-                    existingUser = cookie.getValue();
+            for (Cookie c : cookies) {
+                if (c.getName().equals("user")) {
+                    existingUser = c.getValue();
                 }
-                if (cookie.getName().equals("count")) {
-                    count = Integer.parseInt(cookie.getValue());
+                if (c.getName().equals("count")) {
+                    visitCount = Integer.parseInt(c.getValue());
                 }
             }
         }
 
-        out.println("<html>");
-        out.println("<head><title>Cookie Example</title></head>");
-        out.println("<body style='text-align:center;'>");
+        // If user submits name (first time login)
+        if (userName != null && !userName.isEmpty()) {
 
-        if (existingUser != null) {
+            Cookie userCookie = new Cookie("user", userName);
+            userCookie.setMaxAge(30); // expires in 30 seconds
 
-            count++; // increment
+            Cookie countCookie = new Cookie("count", "1");
+            countCookie.setMaxAge(30);
 
-            // update count cookie
-            Cookie countCookie = new Cookie("count", String.valueOf(count));
-            countCookie.setMaxAge(60);
+            response.addCookie(userCookie);
             response.addCookie(countCookie);
 
-            out.println("<font color=blue><h2>Welcome back, " + existingUser + "!</h2></font>");
-            out.println("<font color=magenta><h2>You have visited this page " + count + " times!</h2></font>");
-            out.println("<p>(Cookie expires in 60 seconds)</p>");
+            response.sendRedirect("CookieServlet");
+            return;
+        }
+
+        // HTML output
+        out.println("<html>");
+        out.println("<head><title>Cookie Example</title></head>");
+        out.println("<body>");
+
+        if (existingUser != null) {
+            visitCount++;
+
+            Cookie countCookie = new Cookie("count", String.valueOf(visitCount));
+            countCookie.setMaxAge(30);
+            response.addCookie(countCookie);
+
+            out.println("<h2 style='color:blue'>Welcome back, " + existingUser + "!</h2>");
+            out.println("<h2 style='color:magenta'>You have visited this page " + visitCount + " times!</h2>");
 
             // Logout button
             out.println("<form action='CookieServlet' method='post'>");
             out.println("<input type='submit' value='Logout'>");
             out.println("</form>");
-        }
 
-        else {
-            out.println("<h2 style='color:red;'>Welcome Guest! you have been logged out or kindly login first time</h2>");
+        } else {
+            out.println("<h2 style='color:red'>Welcome Guest! Please login first time</h2>");
             out.println("<form action='CookieServlet' method='get'>");
             out.println("Enter your name: <input type='text' name='userName'>");
             out.println("<input type='submit' value='Submit'>");
@@ -84,10 +84,10 @@ public class CookieServlet extends HttpServlet {
         out.println("</body></html>");
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // 🔹 Logout → delete cookies
+        // Delete cookies (logout)
         Cookie userCookie = new Cookie("user", "");
         userCookie.setMaxAge(0);
 
